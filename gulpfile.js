9c9b81886810973d063d371f11ebc423706ff156
Harryharries <a1021604754@gmail.com>
2020-05-19 13:54:04 -0500

    add multi tasks running

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 // use 
 const gulp = require('gulp');
-
+const htmlmin = require('gulp-htmlmin');
+const fileinclude = require('gulp-file-include');
+const less = require('gulp-less');
+const csso = require('gulp-csso');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 // gulp.task(): start gulp task
 // parameter
 // 1.task name
@@ -13,3 +18,60 @@ gulp.task('first', (cb) => {
     //callback function for return
     cb()
 });
+
+// html task
+// 1.html compress
+// 2. mutli used html code extraction
+gulp.task('htmlmin', (cb) => {
+    gulp.src('./src/*.html')
+        //mutli used html code extraction
+        .pipe(fileinclude())
+        // compress
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+    cb()
+});
+
+// css任务
+// 1.less语法转换
+// 2.css代码压缩
+gulp.task('cssmin', (cb) => {
+    // 选择css目录下的所有less文件以及css文件
+    gulp.src(['./src/css/*.less', './src/css/*.css'])
+        // 将less语法转换为css语法
+        .pipe(less())
+        // 将css代码进行压缩
+        .pipe(csso())
+        // 将处理的结果进行输出
+        .pipe(gulp.dest('dist/css'))
+    cb()
+});
+
+// js任务
+// 1.es6代码转换
+// 2.代码压缩
+gulp.task('jsmin', (cb) => {
+    gulp.src('./src/js/*.js')
+        .pipe(babel({
+            // 它可以判断当前代码的运行环境 将代码转换为当前运行环境所支持的代码
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+    cb()
+});
+
+
+// 复制文件夹
+gulp.task('copy', (cb) => {
+
+    gulp.src('./src/images/*')
+        .pipe(gulp.dest('dist/images'));
+
+    gulp.src('./src/lib/*')
+        .pipe(gulp.dest('dist/lib'))
+    cb();
+});
+
+// 构建任务
+gulp.task('default', gulp.series(['htmlmin', 'cssmin', 'jsmin', 'copy']));
